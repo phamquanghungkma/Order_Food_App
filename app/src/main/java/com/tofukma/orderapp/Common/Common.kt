5acfe2f0532e2db36cf0f234457f7f482804ab83
quangminh <minhbaljeet2809@gmail.com>
2020-07-26 22:10:06 +0700

    Load Food Size -Minh

--- app/src/main/java/com/tofukma/orderapp/Common/Common.kt
+++ app/src/main/java/com/tofukma/orderapp/Common/Common.kt
@@ -3,8 +3,20 @@ package com.tofukma.orderapp.Common
 import com.tofukma.orderapp.Model.CategoryModel
 import com.tofukma.orderapp.Model.FoodModel
 import com.tofukma.orderapp.Model.UserModel
+import java.math.RoundingMode
+import java.text.DecimalFormat
 
 object Common {
+    fun formatPrice(price: Double): Any {
+        if(price != 0.toDouble())
+        {
+            val df = DecimalFormat("#,##0.0")
+            df.roundingMode = RoundingMode.HALF_UP
+            val finalPrice = StringBuffer(df.format(price)).toString()
+            return finalPrice.replace(".",",")
+        }else
+            return "0,00"
+    }
 
     val COMMENT_REF: String = "Comments"
     var foodSelected:FoodModel ?= null
