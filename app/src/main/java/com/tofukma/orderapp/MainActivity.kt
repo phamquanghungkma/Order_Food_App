2b381af5a105a59e9dbcb26909a89c016db0336f
= <=>
2020-09-24 13:17:05 +0700

    code chua xong

--- app/src/main/java/com/tofukma/orderapp/MainActivity.kt
+++ app/src/main/java/com/tofukma/orderapp/MainActivity.kt
@@ -7,10 +7,17 @@ import android.os.Bundle
 import android.text.TextUtils
 import android.view.LayoutInflater
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
+import com.google.android.gms.common.api.Status
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.api.net.PlacesClient
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener
 
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
@@ -27,9 +34,18 @@ import com.tofukma.orderapp.Model.UserModel
 import dmax.dialog.SpotsDialog
 import io.reactivex.disposables.CompositeDisposable
 import java.util.*
+import kotlin.collections.HashMap
 
 class MainActivity : AppCompatActivity() {
 
+    private var placeSelected: Place?=null
+    private var places_fragment:AutocompleteSupportFragment?=null
+    private lateinit var placeClient:PlacesClient
+    private val placeFields = Arrays.asList(Place.Field.ID,
+    Place.Field.NAME,
+        Place.Field.ADDRESS,
+        Place.Field.LAT_LNG
+        )
     private lateinit var firebaseAuth: FirebaseAuth
     private lateinit var listener: FirebaseAuth.AuthStateListener
     private lateinit var dialog: android.app.AlertDialog
@@ -68,6 +84,10 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun init() {
+
+        Places.initialize(this,getString(R.string.google_maps_key))
+        placeClient = Places.createClient(this)
+
         providers = Arrays.asList<AuthUI.IdpConfig>(AuthUI.IdpConfig.PhoneBuilder().build())
 
         userRef = FirebaseDatabase.getInstance().getReference(Common.USER_REFERENCE)
@@ -136,63 +156,87 @@ class MainActivity : AppCompatActivity() {
             .inflate(R.layout.layout_register, null)
 
         val edt_name = itemView.findViewById<EditText>(R.id.edt_name)
-        val edt_address = itemView.findViewById<EditText>(R.id.edt_address)
-        val edt_phone = itemView.findViewById<EditText>(R.id.edt_phone)
 
+        val edt_phone = itemView.findViewById<EditText>(R.id.edt_phone)
+        val txt_address = itemView.findViewById<TextView>(R.id.txt_address_detail)
+
+        places_fragment = supportFragmentManager.findFragmentById(R.id.places_autocomplete_fragment)
+        as AutocompleteSupportFragment
+        places_fragment!!.setPlaceFields(placeFields)
+        places_fragment!!.setOnPlaceSelectedListener(object:PlaceSelectionListener{
+            override fun onPlaceSelected(p0: Place) {
+                placeSelected = p0
+                txt_address.text = placeSelected!!.address
+            }
+
+            override fun onError(p0: Status) {
+               Toast.makeText(this@MainActivity,""+p0.statusMessage,Toast.LENGTH_SHORT).show()
+            }
+        })
         // set
         edt_phone.setText(user!!.phoneNumber)
         builder.setView(itemView)
         builder.setNegativeButton("CANCEL", { dialogInterface, i -> dialogInterface.dismiss() })
         builder.setPositiveButton("REGISTER", { dialogInterface, i ->
-            if (TextUtils.isDigitsOnly(edt_name.text.toString())) {
-                Toast.makeText(this@MainActivity, "Please enter your name ", Toast.LENGTH_LONG)
-                    .show()
-                return@setPositiveButton
-            } else if (TextUtils.isDigitsOnly(edt_address.text.toString())) {
-                Toast.makeText(this@MainActivity, "Please enter your address ", Toast.LENGTH_LONG)
-                    .show()
-                return@setPositiveButton
-            }
-            val userModel = UserModel()
-            userModel.uid = user!!.uid
-            userModel.name = edt_name.text.toString()
-            userModel.addrss = edt_address.text.toString()
-            userModel.phone = edt_phone.text.toString()
-
-            userRef!!.child(user!!.uid)
-                .setValue(userModel).addOnCompleteListener { task ->
-                    if (task.isSuccessful) {
-                        dialogInterface.dismiss()
-                        Toast.makeText(
-                            this@MainActivity,
-                            "Congratulation! Register Success!",
-                            Toast.LENGTH_LONG
-                        ).show()
-
-                        gotoHomeActivity(userModel)
-                    }
+            if (placeSelected != null) {
+                if (TextUtils.isDigitsOnly(edt_name.text.toString())) {
+                    Toast.makeText(this@MainActivity, "Please enter your name ", Toast.LENGTH_LONG)
+                        .show()
+                    return@setPositiveButton
                 }
-        })
+                val userModel = UserModel()
+                userModel.uid = user!!.uid
+                userModel.name = edt_name.text.toString()
+                userModel.addrss = txt_address.text.toString()
+                userModel.phone = edt_phone.text.toString()
+                userModel.lat = placeSelected!!.latLng!!.latitude
+                userModel.lng = placeSelected!!.latLng!!.longitude
+
+                userRef!!.child(user!!.uid)
+                    .setValue(userModel).addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                           FirebaseAuth.getInstance().currentUser!!
+                               .getIdToken(true)
+                               .addOnFailureListener{t->
+                                   Toast.makeText(this@MainActivity,""+t.message,Toast.LENGTH_SHORT).show()
+                               }
+                               .addOnCompleteListener{
+                                  Common.authorizeToken = it!!.result!!.token
+                                   val headers = HashMap<String,String>()
+                                   headers.put("Authorization",com.google.android.gms.common.internal.service.Common.buildToken(Common.authorizeToken!!))
+                               }
+                            gotoHomeActivity(userModel,braintreeToken.token)
+                        }
+                    }
+            }else{
+              Toast.makeText(this@MainActivity,"Please select address",Toast.LENGTH_SHORT).show()
+            }
+        }
+            )
         //Importan please show dialog
         val dialog = builder.create()
+        dialog.setOnDismissListener{
+            val fragmentTransaction = supportFragmentManager.beginTransaction()
+            fragmentTransaction.remove(places_fragment!!)
+            fragmentTransaction.commit()
+        }
         dialog.show()
 }
 
-    private fun gotoHomeActivity(userModel: UserModel?) {
+    private fun gotoHomeActivity(userModel: UserModel?,token:String?) {
 
         FirebaseInstanceId.getInstance().instanceId.addOnFailureListener { e -> Toast.makeText(this@MainActivity,""+e.message,Toast.LENGTH_SHORT).show()
 
             Common.currentUser = userModel!!
-//            Common.currentToken = token!!
+            Common.currentToken = token!!
 
             startActivity(Intent(this@MainActivity,HomeActivity::class.java))
             finish()
         }
-            .addOnCompleteListener { task -> if (task.isSuccessful){
-
-
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful){
                 Common.currentUser = userModel!!
-//                Common.currentToken = token!!
+                Common.currentToken = token!!
                 Common.updateToken(this@MainActivity,task.result!!.token)
 
                 startActivity(Intent(this@MainActivity,HomeActivity::class.java))
