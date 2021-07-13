0f23248aa22a4556c0a7a490ad41d58c6b3bb11d
phamhung <phamhunggttn@gmail.com>
2021-07-13 17:18:57 +0700

    fix error CommentModel

--- app/src/main/java/com/tofukma/orderapp/Model/CommentModel.kt
+++ app/src/main/java/com/tofukma/orderapp/Model/CommentModel.kt
@@ -1,7 +1,7 @@
 package com.tofukma.orderapp.Model
 
 class CommentModel {
-    var setValueratingValue:Float = 0.toFloat()
+    var ratingValue:Float = 0.toFloat()
     var comment:String?=null
     var name:String ?=null
     var uid:String?=null
